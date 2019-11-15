5fa51c3c13dc9f528514c1e605fa8adfd73f435e
yma67 <yuxiang.ma@mail.mcgill.ca>
2019-11-15 10:23:32 -0800

    rf

--- single_layer_fuse/sfs_api.c
+++ single_layer_fuse/sfs_api.c
@@ -119,7 +119,7 @@ static const pageptr_t PGPTR_NULL = {
 static inline uint16_t dalloc() { 
     SYNCH_BITMAP(read); 
     uint32_t first_free = 0;
-    for (int i = 0; (unsigned)i < super_block.num_data_pages; i++) {
+    for (int i = 1; (unsigned)i < super_block.num_data_pages; i++) {
         if (bitmap[i] == 0) {
             first_free = i;
             break;
